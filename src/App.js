2650b49794484f5541bc97981e2d8c90986dca4a
Nithin R Krishnan <nithin.r@riafy.me>
2022-02-06 18:32:21 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { OTPInput } from "./lib";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <OTPInput />
+    </div>
+  );
 }
 
 export default App;
